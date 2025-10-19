3c0169cfcb7205b3f91dc853c6f935434ccd094d
Kaleab Alemayehu (Neo) <cipherlofi04@gmail.com>
2025-10-19 16:14:21 -0400

    feat: using global css instead of globalstyle using emotion

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { Provider } from "react-redux"
-import { Global } from "@emotion/react"
 import App from "./App"
 import { store } from "./store/store"
-import { globalStyles } from "./styles/globalStyles"
+import "./styles/globals.css"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Global styles={globalStyles} />
       <App />
     </Provider>
   </React.StrictMode>,
